bd0e0d38b249f10fe7b7ec50d9b63f3af6c4d4b8
zhangxiaowei <zhangxiaowei@chinasofti.com>
2019-11-22 20:59:00 +0800

    change some

---
+++ exchange-api/src/test/java/com/github/sawied/microservice/gateway/ExchangeTest.java
@@ -0,0 +1,38 @@
+package com.github.sawied.microservice.gateway;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.github.sawied.microservice.gateway.ews.ExchangeAPI;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ExchangeTest {
+
+	@Autowired
+	private ExchangeAPI exchangeApi;
+	
+	@Test
+	public void aSendMailSuccess() throws InterruptedException {
+		exchangeApi.sendMessage();
+	}
+	
+	@Test
+	public void bMarkMailListSuccess() throws InterruptedException {
+		//exchangeApi.markMailList();
+	}
+	@Test
+	public void cretrieveMarkedMailSuccess() throws InterruptedException {
+		//exchangeApi.retrieveMarkedMailList();
+	}
+	
+
+}
