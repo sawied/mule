88a86d8c6f61d7b07956ba16c2b35f7be3d27f5b
sawied <danan.2009@gmail.com>
2020-02-27 10:31:52 +0800

    change the basic test

---
+++ exchange-api/src/test/java/com/github/sawied/microservice/gateway/BasicConnectionTest.java
@@ -0,0 +1,16 @@
+package com.github.sawied.microservice.gateway;
+
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class BasicConnectionTest {
+
+	@Test
+	public void test() {
+	    Assert.assertTrue("",System.currentTimeMillis() >=  Long.MAX_VALUE);
+	}
+
+}
